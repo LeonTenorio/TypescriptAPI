06d02db28fe3d829269fe8df06eb0648c7d81a83
Leon Tenório <leon_lts@hotmail.com>
2021-08-23 19:12:26 -0300

    feat: context header and authorization getter

--- src/structure/context.ts
+++ src/structure/context.ts
@@ -1,4 +1,5 @@
 import { Express, Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 
 type Method = "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
 
@@ -6,6 +7,7 @@ export default class Context {
   hostname: string;
   url: string;
   body: Record<string, any>;
+  header: IncomingHttpHeaders;
   method: Method;
   params: object;
 
@@ -14,6 +16,7 @@ export default class Context {
   constructor(req: Request) {
     this.hostname = req.hostname;
     this.url = req.url;
+    this.header = req.headers;
     this.body = req.body;
     this.method = req.method as Method;
     if (this.method === undefined || this.method === null)
@@ -29,5 +32,13 @@ export default class Context {
 
   getVariable<T>(key: string): T {
     return this._variables[key] as T;
+  }
+
+  getAuthToken(): string | null {
+    const authField = this.header.authorization;
+    if (authField === undefined) return null;
+    const fields = authField.split("Bearer ");
+    if (fields.length < 2) return null;
+    return fields[1];
   }
 }
