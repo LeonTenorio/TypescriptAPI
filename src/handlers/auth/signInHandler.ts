e3775b60d02f19e78679afe2a735b5f77f4ed016
Leon Tenório <leon_lts@hotmail.com>
2021-08-29 09:41:55 -0300

    feat: ajv validator for login

--- src/handlers/auth/signInHandler.ts
+++ src/handlers/auth/signInHandler.ts
@@ -3,12 +3,21 @@ import { signInWithEmailAndPassword } from '../../services/authentification/fire
 import Context from '../../structure/context';
 import Handler from '../../structure/handler';
 import { NavigationResult } from '../../structure/response';
+import { LoginValidator } from '../../schemas/login';
 
 export const signInHandler = new Handler(
   async (
     context: Context,
     session: ClientSession
   ): Promise<NavigationResult<{ authToken: string }>> => {
+    if (!LoginValidator(context.body as object)) {
+      return {
+        status: 400,
+        body: {
+          error: JSON.stringify(LoginValidator.errors),
+        },
+      };
+    }
     const email = context.body['email'] as string;
     const password = context.body['password'] as string;
 
