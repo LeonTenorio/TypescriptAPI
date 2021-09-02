a5f2b68dd5131ae744183ceaaa3a917d14752050
Leon Tenório <leon_lts@hotmail.com>
2021-09-01 22:52:49 -0300

    test: change email and password

--- src/services/authentification/firebaseAuth.test.ts
+++ src/services/authentification/firebaseAuth.test.ts
@@ -6,6 +6,7 @@ import {
   deleteAccount,
   signInWithEmailAndPassword,
   signOutAllAcounts,
+  updateEmailAndPassword,
 } from './firebaseAuth';
 jest.setTimeout(100000);
 
@@ -59,5 +60,63 @@ test('create an account + revoke token + check token + signIn + delete account',
   if (!signInResult.success) throw signInResult.error;
 
   const deleteAccountResult = await deleteAccount(signInResult.data.token);
+  expect(deleteAccountResult.success).toBe(true);
+
+  const checkLoginTokenAfterDeleteResult = await checkLoginToken(
+    createAccountResult.data.token
+  );
+  expect(checkLoginTokenAfterDeleteResult.success).toBe(false);
+});
+
+test('create an account + update email and password + signIn + delete', async () => {
+  const email = 'test@test.com';
+  const password = 'testtest';
+
+  const createAccountResult = await createAuthAccount(email, password);
+  expect(createAccountResult.success).toBe(true);
+
+  if (!createAccountResult.success) throw createAccountResult.error;
+
+  const updatedEmail = 'test2@test.com';
+  const updatedPassword = 'test2test2';
+  const updateEmailAndPasswordResult = await updateEmailAndPassword(
+    createAccountResult.data.token,
+    updatedEmail,
+    updatedPassword
+  );
+  expect(
+    updateEmailAndPasswordResult.success &&
+      updateEmailAndPasswordResult.data.userId ===
+        createAccountResult.data.userId
+  ).toBe(true);
+  if (!updateEmailAndPasswordResult.success)
+    throw updateEmailAndPasswordResult.error;
+
+  const checkLoginTokenResult = await checkLoginToken(
+    updateEmailAndPasswordResult.data.token
+  );
+  expect(
+    checkLoginTokenResult.success &&
+      checkLoginTokenResult.data.email === updatedEmail
+  ).toBe(true);
+
+  const signInPreviousAccountResult = await signInWithEmailAndPassword(
+    email,
+    password
+  );
+  expect(signInPreviousAccountResult.success).toBe(false);
+
+  const signInResult = await signInWithEmailAndPassword(
+    updatedEmail,
+    updatedPassword
+  );
+  expect(
+    signInResult.success &&
+      signInResult.data.userId === updateEmailAndPasswordResult.data.userId
+  ).toBe(true);
+
+  const deleteAccountResult = await deleteAccount(
+    updateEmailAndPasswordResult.data.token
+  );
   expect(deleteAccountResult.success).toBe(true);
 });
