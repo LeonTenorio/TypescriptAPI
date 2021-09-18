6e05127430b2d6334154c3fc786b02d4da842632
Leon Tenório <leon_lts@hotmail.com>
2021-09-18 12:49:11 -0300

    feat: prod, beta and local .env configuration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-require('dotenv').config();
-
 import express from 'express';
 import { json } from 'body-parser';
 import { connectDatabase } from './config/database';
 import { router as dummyRouter } from './router/dummy';
 import { router as authRouter } from './router/auth';
+import { Command } from 'commander';
+import dotenv from 'dotenv';
 
-// require("./services/authentification/firebaseAuth.local");
+const program = new Command();
+
+program.option('-p, --prod', 'production env');
+program.option('-b, --beta', 'beta env');
+program.option('-l, --local', 'local env');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.prod) {
+  dotenv.config({ path: '.env.prod' });
+} else if (options.beta) {
+  dotenv.config({ path: '.env.beta' });
+} else {
+  dotenv.config({ path: '.env' });
+}
 
 const app = express();
 app.use(json({ limit: '50mb' }));
