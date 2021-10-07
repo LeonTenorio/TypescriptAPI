7d9e43d034a5e127f1c0abf84ecaf374c89eba03
Leon Tenório <leon_lts@hotmail.com>
2021-10-07 18:43:31 -0300

    feat: connect database before start app

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import { connectDatabase } from './config/database';
 import { router as dummyRouter } from './router/dummy';
 import { router as authRouter } from './router/auth';
 
-const app = express();
-app.use(json({ limit: '50mb' }));
-
-app.use(dummyRouter);
-app.use(authRouter);
-
-app.listen(3000, async () => {
-  console.log('server is listening on port 3000');
-  await connectDatabase();
+connectDatabase().then(() => {
   console.log('connected to mongodb');
+
+  const app = express();
+  app.use(json({ limit: '50mb' }));
+
+  app.use(dummyRouter);
+  app.use(authRouter);
+
+  app.listen(3000, async () => {
+    console.log('server is listening on port 3000');
+  });
 });
