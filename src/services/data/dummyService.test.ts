53aaa6a41925173563775a2f8064495d461aac2e
Leon Tenório <leon_lts@hotmail.com>
2021-10-27 22:11:43 -0300

    feat: rollback test

--- src/services/data/dummyService.test.ts
+++ src/services/data/dummyService.test.ts
@@ -6,23 +6,51 @@ import {
 
 jest.setTimeout(10000);
 
-test('', async () => {
-  const service: DatabaseService<void> = async (db, session) => {
-    const dummyData = { message: 'test', time: new Date() };
+let dummyDataAmount: number | undefined = undefined;
 
-    const readBeforeInsertion = await dummyService.readDummyDatas(db, session);
-    expect(readBeforeInsertion.success).toBe(true);
+test('try to insert a dummy data, read the dummy datas and rollback this', async () => {
+  const test = <T>(computeFirstReadLength?: (value: number) => void) => {
+    const service: DatabaseService<void> = async (db, session) => {
+      const dummyData = { message: 'test', time: new Date() };
 
-    const addResult = await dummyService.addDymmyData(dummyData, db, session);
-    expect(addResult.success).toBe(true);
+      const readBeforeInsertion = await dummyService.readDummyDatas(
+        db,
+        session
+      );
+      expect(readBeforeInsertion.success).toBe(true);
 
-    const readAfterInsertion = await dummyService.readDummyDatas(db, session);
-    expect(
-      readAfterInsertion.success &&
-        readBeforeInsertion.success &&
-        readAfterInsertion.data.length - readBeforeInsertion.data.length === 1
-    ).toBe(true);
+      if (!readBeforeInsertion.success) throw readBeforeInsertion.error;
+
+      if (computeFirstReadLength !== undefined) {
+        computeFirstReadLength(readBeforeInsertion.data.length);
+      }
+
+      const addResult = await dummyService.addDymmyData(dummyData, db, session);
+      expect(addResult.success).toBe(true);
+
+      const readAfterInsertion = await dummyService.readDummyDatas(db, session);
+      expect(
+        readAfterInsertion.success &&
+          readAfterInsertion.data.length - readBeforeInsertion.data.length === 1
+      ).toBe(true);
+    };
+    return service;
   };
 
-  await withDatabaseTransaction(service, undefined, true);
+  let readBeforeInsertionAmount: number | undefined = undefined;
+  await withDatabaseTransaction(
+    test((value) => {
+      readBeforeInsertionAmount = value;
+    }),
+    undefined,
+    true
+  );
+
+  await withDatabaseTransaction(
+    test((value) => {
+      expect(value === readBeforeInsertionAmount).toBe(true);
+    }),
+    undefined,
+    true
+  );
 });
